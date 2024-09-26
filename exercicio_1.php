65769b47adaf11245db6a62a200702d34bad4940
MarieAlcantara <mariellyalcantara06@gmail.com>
2024-09-26 11:05:37 -0300

    first commit

---
+++ exercicio_1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 1</title>
+</head>
+<body>
+    <?php 
+    echo "<h1>Imagem<h1>";
+    echo "<img src='https://picsum.photos/500/600' alt='img' >";
+    ?>
+</body>
+</html>
+
---
+++ exercicio_2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo a href="http:///www.google.com.br/?hl=pt-BR"</a>
+    ?>
+</body>
+</html>
