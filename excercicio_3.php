6bc67fc3f615eaeb6cab274e053a20553338c816
MarieAlcantara <101595804+MarieAlcantara@users.noreply.github.com>
2024-10-03 12:36:37 -0300

    ex 3

--- excercicio_3.php
+++ excercicio_3.php
@@ -6,13 +6,18 @@
     <title>Exercício 3</title>
 </head>
 <body>
-    <img src="" alt="Imagem dia">
     <?php
     $datetime = new DateTime( "now", new DateTimeZone( "America/Sao_Paulo" ) );
     $hora = $datetime->format( 'H' );
-    if ($hora >=0 & <=12) {
+    if ($hora >=0 && $hora<=12) {
         echo "<h1>Bom Dia!</h1></br>";
-        echo 
+        echo "<img src='https://www.portalriomaina.com/wp-content/uploads/2022/05/dia-do-sol.jpg' alt='Imagem dia'>";
+    } elseif ($hora >12 && $hora<18){
+        echo "<h1>Boa Tarde!</h1></br>";
+        echo "<img src='https://img.freepik.com/fotos-premium/fim-da-tarde-natureza-paisagem-foto-de-um-rio-ao-por-do-sol_566982-137.jpg' alt='Imagem tarde'>";
+    } else{
+        echo "<h1>Boa Noite!</h1></br>";
+        echo "<img src='https://borealisexpedicoes.com.br/wp-content/uploads/lofoten-noruega-noite-polar-1-scaled.jpg' alt='Imagem noite'>";
     }
     ?>
 </body>
